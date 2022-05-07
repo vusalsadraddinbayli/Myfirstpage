6fc53309144c5c91981dd23864d3b5fa8fa1bfca
kargo <mahmudovarahide@gmail.com>
2022-05-07 11:51:36 +0400

    js

---
+++ JS ilk/main.js
@@ -0,0 +1,69 @@
+// let
+// var
+
+// const
+
+//PRIMITE DATATYPES==============================================================================
+//number
+let vusal = 3;
+var rahide = 8;
+const error = 14;
+
+var topla = vusal + rahide + " " + "toplandim";
+var cix = rahide - vusal + " " + "cixildim";
+// vurma *
+// bolme /
+//qaliq %
+
+var bolme = rahide % vusal;
+
+// const error = 15;//constda eyni adi cagirmaq olmaz consta mudaxile etmek olmaz
+
+var rahide = 12;
+
+console.log(typeof vusal);
+console.log(rahide);
+console.log(error);
+
+console.log(topla)
+console.log(cix)
+console.log(bolme)
+
+//string
+let rahideniAdi = "rahide";
+console.log(typeof rahideniAdi);
+
+let vusalinAdi = "Vusal";
+console.log(vusalinAdi);
+
+
+
+//boolean
+var dogrudumu = false;
+
+console.log(typeof dogrudumu);
+
+//undifind
+
+var val;
+console.log(val)
+
+
+//====================================================================================================
+
+
+
+
+//None PRIMITE DATA TYPES============================================================================
+
+var arry = ["BWM", "MERCEDES", "VOLVO"];
+var arry2 = ["Vusal", "Rahide", "Nuray"];
+var arryToplam = arry + arry2;
+console.log(arry);
+console.log(arry.length);
+console.log(arry[0]);
+console.log(arry[1]);
+console.log(arry[2]);
+console.log(arryToplam)
+
+//===================================================================================================
