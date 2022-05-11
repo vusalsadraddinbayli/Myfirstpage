b59306566da1ba3552b20230dbb71bea3ff7772f
vusalsadraddinbayli <vsadraddinbeyli@mail.ru>
2022-05-11 22:16:31 +0400

    js1

---
+++ JS first task/main.js
@@ -0,0 +1,24 @@
+var x = 21;
+var y = 7;
+var toplama = x + y;
+var cixma = x - y;
+var vurma = x * y;
+var bolme = x / y;
+console.log(x);
+console.log(y);
+console.log(toplama);
+console.log(cixma);
+console.log(vurma);
+console.log(bolme);
+
+let bolmeninisaresi = "/";
+console.log(bolmeninisaresi);
+console.log(typeof bolmeninisaresi);
+
+var arry = ["A", "B", "C", "D"];
+console.log(arry);
+console.log(arry.length);
+console.log(arry[0]);
+console.log(arry[1]);
+console.log(arry[2]);
+console.log(arry[3]);
